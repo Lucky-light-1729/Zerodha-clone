9f12d22f9ade311ecb8a40932ec59d8eca82144d
Vishal-Parihar-8104 <wwwvishalkumar872@gmail.com>
2025-07-02 07:58:09 +0530

    add by vishal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
-import './App.css'
-import Navbar from "./Navbar";
-import Policies from "./Component/Policies";
-import Disclousure from './Component/Disclousure';
-import Charges from "./Component/Charges";
-import Market from "./Component/Market-holiday";
-import Investor from "./Component/Investor";
+import './App.css
+import Navbar from './Navbar'
+import FooterLink from './FooterLink'
+import  ScrollToTop from './ScrollToTop'
 
 function App() {
 
   return (
     <div>
-   <Navbar />
-   {/* <Policies /> */}
-   {/* <Disclousure /> */}
-   {/* <Charges /> */}
-   {/* <Market /> */}
-   <Investor />
+      <ScrollToTop />
+      <Navbar/>
+      <FooterLink/> 
     </div>
   )
 }
